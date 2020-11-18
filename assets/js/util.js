60305bfcdd59709dfce434755170d1c2baeca13b
Hafiz Ahmad Hassan <31056702+HafizAhmadHassan@users.noreply.github.com>
2020-11-18 09:14:54 +0100

    Update util.js

--- assets/js/util.js
+++ assets/js/util.js
@@ -517,21 +517,7 @@
 		return $this;
 
 	};
--function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less"; 
-    moreText.style.display = "inline";
-  }
-}
 
 	/**
 	 * Moves elements to/from the first positions of their respective parents.
